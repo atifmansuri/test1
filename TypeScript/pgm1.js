b20525aae2b85333711e7b20817bb993ab1b9348
atifmansuri <57576283+atifmansuri@users.noreply.github.com>
2022-09-07 16:25:43 +0530

    test

---
+++ TypeScript/pgm1.js
@@ -0,0 +1,6 @@
+"use strict";
+//This is a TypeScript
+var num1 = 10;
+var num2 = 20;
+console.log(num1);
+console.log(num2);
---
+++ TypeScript/pgm1.ts
@@ -0,0 +1,5 @@
+//This is a TypeScript
+var num1 : number = 10;
+var num2 = 20;
+console.log(num1);
+console.log(num2);
